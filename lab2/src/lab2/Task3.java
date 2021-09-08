3662942efbf86b751691788ebd1adb948a6d61cc
dmitriynaumov1024 <naumov1024@gmail.com>
2021-09-08 11:04:05 +0300

    Lab2: tsk3: move creation of randomized int array to a separate method

--- lab2/src/lab2/Task3.java
+++ lab2/src/lab2/Task3.java
@@ -9,6 +9,20 @@ import java.util.Random;
  */
 public class Task3 implements ITask {
     final static String helpString = "Transpose randomly populated matrix of given size.\nUsage: <size>";
+    
+    static int[][] randomArray2d(int rowCount, int columnCount, int lowerValueBound, int upperValueBound){
+        int[][] result = new int[rowCount][columnCount];
+        int valueRange = Math.abs(upperValueBound - lowerValueBound);
+        int padding = Math.min(lowerValueBound, upperValueBound);
+        Random random = new Random();
+        for(int[] row : result){
+            for(int i=0; i<row.length; i++){
+                row[i] = random.nextInt(valueRange) + padding;
+            }
+        }
+        return result;
+    }
+    
     public void run(String[] args){
         if(args.length < 1){
             System.out.println(helpString);
@@ -16,14 +30,8 @@ public class Task3 implements ITask {
         }
         
         int size = Integer.parseInt(args[0]);
-        Random random = new Random();
-        int randomLimit = size * 2;
-        int[][] matrix = new int[size][size];
-        for (int[] row : matrix){
-            for (int i=0; i<row.length; i++){
-                row[i] = random.nextInt(randomLimit);
-            }
-        }
+        
+        int[][] matrix = randomArray2d(size, size, 0, size * 2);
         
         int[][] transposedMatrix = Transposer.transpose(matrix);
         
