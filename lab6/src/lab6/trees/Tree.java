788405430d656aea96d49ec7797a428c9b88bdf1
dmitriynaumov1024 <naumov1024@gmail.com>
2021-11-08 10:36:04 +0200

    Lab6: Tree: implement methods

--- lab6/src/lab6/trees/Tree.java
+++ lab6/src/lab6/trees/Tree.java
@@ -1,5 +1,7 @@
 package lab6.trees;
 
+import java.util.Stack;
+
 /**
  * A binary tree without any values.
  * @author Dmitriy Naumov
@@ -7,6 +9,14 @@ package lab6.trees;
 public class Tree {
     
     private TreeNode root;
+    
+    /**
+     * Create new, empty Tree.
+     */
+    public Tree () {
+        this.root = new TreeNode();
+        this.root.childL = root;
+    }
     
     /**
      * Make this Tree full (all nodes with 2 children are placed contigously, 
@@ -25,22 +35,98 @@ public class Tree {
     }
     
     /**
-     * Checks if there is a node in this Tree at given depth and 
+     * Check if there is a node in this Tree at given depth and 
      * horizontal position.
      * @param depth depth of required node.
      * @param horizontal horizontal position of required node.
-     * @return 
+     * @return if there is a node at given depth and horizontal position.
      */
     public final boolean hasNodeAt (int depth, int horizontal) {
-        return false;
+        try {
+            return this.getNode(address(depth, horizontal)) != null;
+        }
+        finally {
+            return false;
+        }
     }
     
-    public void addNodeAt (int depth, int horizontal) {
-        
+    /**
+     * Check if node can be added at given position. To add node, there must 
+     * be path to the node and given position must not be occupied. 
+     * @param depth depth of required node.
+     * @param horizontal horizontal position of required node.
+     * @return if node can be added in given position.
+     */
+    public final boolean canAddNodeAt (int depth, int horizontal) {
+        try {
+            return this.getNode(address(depth, horizontal) / 2)
+                       .getChild(horizontal % 2) == null;
+        }
+        finally {
+            return false;
+        }
     }
     
-    public void removeNodeAt (int depth, int horizontal) {
-        
+    /**
+     * Create new node at given depth-horizontal address only if the
+     * address is accessible by already existing path and the address is not 
+     * taken. Otherwise this method will throw TreeException.
+     * @param depth depth of required node.
+     * @param horizontal horizontal position of required node.
+     * @throws TreeException
+     */
+    public final void addNodeAt (int depth, int horizontal) throws TreeException {
+        TreeNode node = this.getNode(address(depth, horizontal) / 2);
+        if (node.getChild(horizontal % 2) != null) {
+            throw new TreeException (TreeException.NODE_OCCUPIED);
+        }
+        node.setChild(horizontal % 2, new TreeNode());
     }
     
+    /**
+     * Remove node at given depth-horizontal address only if there is a node.
+     * Otherwise this method will throw TreeException.
+     * @param depth depth of required node.
+     * @param horizontal horizontal position of required node.
+     * @throws TreeException
+     */
+    public void removeNodeAt (int depth, int horizontal) throws TreeException {
+        TreeNode node = this.getNode(address(depth, horizontal) / 2);
+        if (node.getChild(horizontal % 2) == null) {
+            throw new TreeException (TreeException.NODE_NOTFOUND);
+        }
+        node.setChild(horizontal % 2, null);
+    }
+    
+    protected TreeNode getNode (int address) throws TreeException {
+        TreeNode current = this.root;
+        for (Boolean waypoint : getPath(address)) {
+            current = current.getChild(waypoint);
+            if (current == null){
+                throw new TreeException (TreeException.NODE_UNREACHABLE);
+            }
+        }
+        return current;
+    }
+    
+    @Override protected void finalize() throws Throwable {
+        if (this.root != null) this.root.childL = null;
+        super.finalize();
+    }
+    
+    static Stack<Boolean> getPath (int address) {
+        if (address <= 0){
+            return new Stack<>();
+        }
+        Stack<Boolean> result = new Stack<>();
+        while (address > 0) {
+            result.push((address % 2 == 0) ? Boolean.FALSE : Boolean.TRUE);
+            address /= 2;
+        }
+        return result;
+    }
+    
+    static int address (int depth, int horizontal) {
+        return 1 << depth + horizontal;
+    }
 }
