2fb096e22df577ff75741c375807f00487d620a0
dmitriynaumov1024 <naumov1024@gmail.com>
2021-11-10 17:14:56 +0200

    Lab6: TreeException: set up getMessage method, add new variants

--- lab6/src/lab6/trees/TreeException.java
+++ lab6/src/lab6/trees/TreeException.java
@@ -18,9 +18,25 @@ public class TreeException extends Exception {
         this.variant = variant;
     }
     
+    @Override public String getMessage () {
+        switch (this.variant) {
+            case NODE_UNREACHABLE:
+                return "Node is unreachable.";
+            case NODE_OCCUPIED:
+                return "Node is already occupied";
+            case NODE_NOTFOUND:
+                return "Node can not be found.";
+            case NODE_EXISTS:
+                return "Node already exists.";
+            default:
+                return "Tree exception";
+        }
+    }
+    
     public final static int
         UNKNOWN             = 0,
         NODE_UNREACHABLE    = 1,
         NODE_OCCUPIED       = 2,
-        NODE_NOTFOUND       = 3;
+        NODE_NOTFOUND       = 3,
+        NODE_EXISTS         = 4;
 }
