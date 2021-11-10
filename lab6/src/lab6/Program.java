5a582c0f05971c9e9da15bf130d3d896ec518f08
dmitriynaumov1024 <naumov1024@gmail.com>
2021-11-10 17:17:41 +0200

    Lab6: Program: add some Tree<T> examples

--- lab6/src/lab6/Program.java
+++ lab6/src/lab6/Program.java
@@ -1,5 +1,7 @@
 package lab6;
 
+import lab6.trees.generic.*;
+
 /**
  * Main class
  * @author Dmitriy Naumov
@@ -11,6 +13,63 @@ public class Program {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Tree<Integer> tree = new Tree<>();
+        try {
+            int d = 0, h = 0;
+            tree.addNodeAt(d, h);
+            tree.setValueAt(d, h, Integer.MIN_VALUE);
+            System.out.printf("Value at [%d:%d] = %d \n", d, h, tree.getValueAt(d, h));
+            
+            d = 1; h = 0;
+            System.out.printf("Can I add node at [%d:%d]? %s \n", d, h, tree.canAddNodeAt(d, h));
+            tree.addNodeAt(d, h);
+            tree.setValueAt(d, h, Integer.MIN_VALUE + d);
+            System.out.printf("Value at [%d:%d] = %d \n", d, h, tree.getValueAt(d, h));
+            
+            d = 0; h = 0;
+            System.out.printf("Value at [%d:%d] = %d \n", d, h, tree.getValueAt(d, h));
+            
+            
+            d = 1; h = 1;
+            System.out.printf("Can I add node at [%d:%d]? %s \n", d, h, tree.canAddNodeAt(d, h));
+            tree.addNodeAt(d, h);
+            tree.setValueAt(d, h, 444);
+            System.out.printf("Value at [%d:%d] = %d \n", d, h, tree.getValueAt(d, h));
+            
+            System.out.printf("Is this Tree full/complete? %s \n", tree.checkCompletion());
+            
+            tree.removeNodeAt(d, h);
+            System.out.printf("Removed node [%d:%d] \n", d, h);
+            System.out.printf("Is this Tree full/complete? %s \n", tree.checkCompletion());
+            
+            d = 2; h = 0;
+            tree.addNodeAt(d, h);
+            tree.setValueAt(d, h, 456);
+            System.out.printf("Added node [%d:%d] \n", d, h);
+            System.out.printf("Value at [%d:%d] = %d \n", d, h, tree.getValueAt(d, h));
+            System.out.printf("Is this Tree full/complete? %s \n", tree.checkCompletion());
+            
+            d = 1; h = 1;
+            tree.addNodeAt(d, h);
+            tree.setValueAt(d, h, -200);
+            System.out.printf("Added node [%d:%d] \n", d, h);
+            System.out.printf("Value at [%d:%d] = %d \n", d, h, tree.getValueAt(d, h));
+            System.out.printf("Is this Tree full/complete? %s \n", tree.checkCompletion());
+            
+            d = 2; h = 3;
+            tree.addNodeAt(d, h);
+            tree.setValueAt(d, h, 324);
+            System.out.printf("Added node [%d:%d] \n", d, h);
+            System.out.printf("Value at [%d:%d] = %d \n", d, h, tree.getValueAt(d, h));
+            System.out.printf("Is this Tree full/complete? %s \n", tree.checkCompletion());
+            
+            System.out.printf ("%s \n", tree);
+            
+            
+        }
+        catch (Exception ex) {
+            System.out.printf ("Exception: %s \n", ex.getMessage());
+        }
         
     }
     
