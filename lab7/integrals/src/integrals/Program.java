a04ba6465cad4d15d076c5c80df1e11f256e0880
dmitriynaumov1024 <naumov1024@gmail.com>
2021-11-11 19:36:58 +0200

    Create Lab7

---
+++ lab7/integrals/src/integrals/Program.java
@@ -0,0 +1,17 @@
+package integrals;
+
+/**
+ * Main class
+ * @author Dmitriy Naumov
+ */
+public class Program {
+
+    /**
+     * Entry point
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
