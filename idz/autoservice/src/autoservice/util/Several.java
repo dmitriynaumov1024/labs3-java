7d9d4a94db62eed78a4b52baaf5ed02fbd74486e
dmitriynaumov1024 <naumov1024@gmail.com>
2021-12-02 10:21:33 +0200

    Individual task: add util class Several

---
+++ idz/autoservice/src/autoservice/util/Several.java
@@ -0,0 +1,11 @@
+package autoservice.util;
+
+/**
+ * A class to represent several identical items.
+ * @param <T> item type
+ * @author Dmitriy Naumov
+ */
+public class Several<T> {
+    public T value;
+    public int count;
+}
