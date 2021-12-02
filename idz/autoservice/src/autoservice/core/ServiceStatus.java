508b076d16dd94fa4e8b4b8c81df1007b63f89df
dmitriynaumov1024 <naumov1024@gmail.com>
2021-12-02 20:57:27 +0200

    Individual task: add ServiceStatus

---
+++ idz/autoservice/src/autoservice/core/ServiceStatus.java
@@ -0,0 +1,11 @@
+package autoservice.core;
+
+/**
+ * Service action status
+ * @author Dmitriy Naumov
+ */
+public enum ServiceStatus implements java.io.Serializable {
+    Planned,
+    InProgress,
+    Finished
+}
