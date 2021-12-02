405a323145a9153717dbe40807a2310cf9583c7a
dmitriynaumov1024 <naumov1024@gmail.com>
2021-12-02 20:58:00 +0200

    Individual task: Worker now extends Person, add hourSalary field

--- idz/autoservice/src/autoservice/core/Worker.java
+++ idz/autoservice/src/autoservice/core/Worker.java
@@ -6,8 +6,9 @@ import java.util.*;
  * Class that contains info about autoservice worker.
  * @author Dmitriy Naumov
  */
-public class Worker {
+public class Worker extends Person {
     public List<CarTag> preferences;
+    public int hourSalary;
     
     public Worker () {
         super();
