82e2fa52cb31d37b02bf6ef41b14f247661a24f8
dmitriynaumov1024 <naumov1024@gmail.com>
2021-12-02 20:56:24 +0200

    Individual task: make CarPart serializable, add constructors

--- idz/autoservice/src/autoservice/core/CarPart.java
+++ idz/autoservice/src/autoservice/core/CarPart.java
@@ -4,9 +4,18 @@ package autoservice.core;
  * Class that contains car part info.
  * @author Dmitriy Naumov
  */
-public class CarPart {
+public class CarPart implements java.io.Serializable {
     public String brand;
     public String name;
     public String description;
     public int price;
+    
+    public CarPart () { }
+    
+    public CarPart (String brand, String name, String description, int price) {
+        this.brand = brand;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
 }
