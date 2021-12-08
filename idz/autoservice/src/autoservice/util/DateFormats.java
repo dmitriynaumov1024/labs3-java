63311a531418daf4cb75e7388bfc19c2c64d6a77
dmitriynaumov1024 <naumov1024@gmail.com>
2021-12-08 15:09:30 +0200

    Individual: DateFormats: add yyyy-MM-dd hh:mm formatter

--- idz/autoservice/src/autoservice/util/DateFormats.java
+++ idz/autoservice/src/autoservice/util/DateFormats.java
@@ -11,6 +11,11 @@ public class DateFormats {
     public static SimpleDateFormat yyyy_mm_dd () {
         return _yyyy_mm_dd;
     }
+    
+    public static SimpleDateFormat yyyy_mm_dd_time () {
+        return _yyyy_mm_dd_time;
+    }
             
     private static SimpleDateFormat _yyyy_mm_dd = new SimpleDateFormat("yyyy-MM-dd");
+    private static SimpleDateFormat _yyyy_mm_dd_time = new SimpleDateFormat("yyyy-MM-dd hh:mm");
 }
